b5a9d82c35ad6186f21929c950ae8771daf922b9
Vasil Vasilev <backort@abv.bg>
2022-01-20 20:54:03 +0200

    card

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,13 +1,20 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
 import User from "../../src/components/user/User";
-import Avatar  from "../../src/components/avatar/Avatar";
+import Avatar from "../../src/components/avatar/Avatar";
+import Card from "../../src/components/card/Card";
 
 export default function About() {
   return (
     <Container maxWidth="sm">
-      <User />
-      <Avatar url={'/images/avatar.png'} />
+      <Card
+        name="terlik"
+        likes={2230}
+        mediaUrl="/images/nft.jpg"
+        user={{ avatar: "/images/avatar.png", verified: true }}
+        price="10"
+        currency="ETH"
+      />
     </Container>
   );
 }
---
+++ src/components/card/Card.js
@@ -0,0 +1,49 @@
+import styles from "./Card.module.scss";
+import millify from "millify";
+import classNames from "classnames";
+import Avatar from "../avatar/Avatar";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Chip,
+  Typography,
+} from "@mui/material";
+
+export default function CardComponent({
+  name,
+  likes,
+  mediaUrl,
+  user,
+  price,
+  currency,
+}) {
+  return (
+    <Card sx={{ maxWidth: 345 }} className={classNames(styles.card)}>
+      <CardHeader
+        avatar={<Avatar url={user.avatar} verified={user.verified} size={55} />}
+      />
+      <CardMedia
+        className={classNames(styles.media)}
+        component="img"
+        image={mediaUrl}
+        alt={mediaUrl}
+      />
+      <CardContent className={classNames(styles.titleContainer)}>
+        <div>
+          <Typography className={classNames(styles.title)}>{name}</Typography>
+          <Typography className={classNames(styles.price)}>
+            {price} {currency}
+          </Typography>
+        </div>
+        <Chip
+          label={millify(likes)}
+          variant="outlined"
+          className={classNames(styles.likes)}
+        />
+      </CardContent>
+    </Card>
+  );
+}

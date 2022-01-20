6bd79b8db91cbae76ec7210d56c15a7d72ca5177
Vasil Vasilev <backort@abv.bg>
2022-01-20 23:34:32 +0200

    live auctions component

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -4,6 +4,7 @@ import User from "../../src/components/user/User";
 import Avatar from "../../src/components/avatar/Avatar";
 import Card from "../../src/components/card/Card";
 import Trending from "../../src/components/trending/Trending";
+import Auctions from "../../src/components/auctions/Auctions";
 
 export default function About() {
   const cards = [
@@ -54,7 +55,8 @@ export default function About() {
       mediaUrl: "images/nft.jpg",
       price: 10,
       currency: "ETH",
+      timeLeft: 3000
     },
   ];
-  return <Trending cards={cards} />;
+  return <Auctions cards={cards} />;
 }
---
+++ src/components/auctions/Auctions.jsx
@@ -0,0 +1,47 @@
+import styles from "./Auctions.module.scss";
+import {
+  MenuItem,
+  Select,
+  Typography,
+  Container,
+  Grid,
+  FormControl,
+} from "@mui/material";
+import classNames from "classnames";
+import CardComponent from "../card/Card";
+
+export default function Auctions({ cards = [] }) {
+  return (
+    <Container maxWidth="xl">
+      <div className={classNames(styles.header)}>
+        <Typography variant="h1">&#128293; Live Auctions</Typography>
+        <FormControl sx={{ m: 1, minWidth: 200 }}>
+          <Select displayEmpty>
+            <MenuItem>This week</MenuItem>
+            <MenuItem>This month</MenuItem>
+            <MenuItem>This year</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <Container maxWidth="xl">
+        <Grid container spacing={2}>
+          {cards.map((card, index) => (
+            <Grid key={index} item xs={12} md={3} lg={3}>
+              <CardComponent
+                name={card.name}
+                user={{
+                  avatar: card.user.avatar.url,
+                  verified: card.user.verified,
+                }}
+                mediaUrl={card.mediaUrl}
+                price={card.price}
+                currency={card.currency}
+                timeLeft={card.timeLeft}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Container>
+  );
+}

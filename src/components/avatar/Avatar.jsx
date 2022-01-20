b872e98d74de019d95570b948b42220ffa09180e
Vasil Vasilev <backort@abv.bg>
2022-01-20 17:57:09 +0200

    avatar

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,6 +1,16 @@
 import styles from "./Avatar.module.scss";
 
 export default function Avatar({ size, url, verified }) {
+  if (size) {
+    size = size;
+  } else {
+    size = 90;
+  }
+  if (verified) {
+    verified = verified;
+  } else {
+    verified = false;
+  }
   return (
     <div
       className={styles.avatar}

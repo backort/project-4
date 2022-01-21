1db2a35a3d98ef8bb604c019e09ceddd34069084
Vasil Vasilev <backort@abv.bg>
2022-01-21 19:45:06 +0200

    fixed steps

--- src/components/how/How.jsx
+++ src/components/how/How.jsx
@@ -1,4 +1,4 @@
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Container, Grid, Typography } from "@mui/material";
 import styles from "./How.module.scss";
 import classNames from "classnames";
 import Step from "./Step";
@@ -6,37 +6,39 @@ import Link from "next/link";
 
 export default function How({ description, title, items = [], link }) {
   return (
-    <Grid className={classNames(styles.howContainer)}>
-      <div className={classNames(styles.textContainer)}>
-        <Grid item>
-          <Typography variant="h2" className={classNames(styles.title)}>
-            {title}
-          </Typography>
-          <Typography paragraph className={classNames(styles.description)}>
-            {description}
-          </Typography>
+    <div className={classNames(styles.wrapper)}>
+      <Grid className={classNames(styles.howContainer)}>
+        <div className={classNames(styles.textContainer)}>
+          <Grid item>
+            <Typography variant="h2" className={classNames(styles.title)}>
+              {title}
+            </Typography>
+            <Typography paragraph className={classNames(styles.description)}>
+              {description}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Link href={link}>
+              <Button
+                className={classNames(styles.btn)}
+                component="a"
+                variant="contained"
+              >
+                LEARN MORE
+              </Button>
+            </Link>
+          </Grid>
+        </div>
+        <Grid item className={classNames(styles.steps)}>
+          {items.map((item, index) => (
+            <Step
+              title={item.title}
+              description={item.description}
+              number={index + 1}
+            />
+          ))}
         </Grid>
-        <Grid item>
-          <Link href={link}>
-            <Button
-              className={classNames(styles.btn)}
-              component="a"
-              variant="contained"
-            >
-              LEARN MORE
-            </Button>
-          </Link>
-        </Grid>
-      </div>
-      <Grid item className={classNames(styles.steps)}>
-        {items.map((item, index) => (
-          <Step
-            title={item.title}
-            description={item.description}
-            number={index + 1}
-          />
-        ))}
       </Grid>
-    </Grid>
+    </div>
   );
 }

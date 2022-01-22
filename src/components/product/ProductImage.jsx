c73b2b0593b5f1563123999bb85d6c64fd98c63c
Vasil Vasilev <backort@abv.bg>
2022-01-22 02:43:13 +0200

    class

--- src/components/product/ProductImage.jsx
+++ src/components/product/ProductImage.jsx
@@ -3,7 +3,7 @@ import classNames from "classnames";
 
 export default function ProductImage({ url }) {
   return (
-    <div className={classNames(styles.product-image)}>
+    <div className={classNames(styles.productImage)}>
       <img src={url} className={classNames(styles.image)} />
     </div>
   );

686e973cfe4017508fb7b1f693a9c3dd0db2039f
Vasil Vasilev <backort@abv.bg>
2022-01-22 02:34:20 +0200

    fix class

--- src/components/productimage/ProductImage.jsx
+++ src/components/productimage/ProductImage.jsx
@@ -3,8 +3,8 @@ import classNames from "classnames";
 
 export default function ProductImage({ url }) {
   return (
-    <div className={classNames(style.product-image)}>
-      <img src={url} className={classNames(style.image)} />
+    <div className={classNames(styles.product-image)}>
+      <img src={url} className={classNames(styles.image)} />
     </div>
   );
 }

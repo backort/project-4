e09c181891de66488218c5029eea85869d8c525a
Vasil Vasilev <backort@abv.bg>
2022-01-26 18:33:22 +0200

    a

--- src/components/product/ProductInfoTimer.jsx
+++ src/components/product/ProductInfoTimer.jsx
@@ -4,15 +4,18 @@ import Countdown from "react-countdown";
 import React from "react";
 
 export default function ProductInfoTimer({ timeEnd, onTimeEnd }) {
-  if (timeEnd) {
-    return (
-      <div className={classNames(styles["product-info-timer"])}>
-        <p className={classNames(styles.title)}>Ends in</p>
-        <div className={classNames(styles.timer)}>
-          <Countdown date={onTimeEnd} />
-        </div>
-      </div>
-    );
-  }
-  return <React.Fragment></React.Fragment>;
+  return (
+    <div className={classNames(styles["product-info-timer"])}>
+      {timeEnd ? (
+        <React.Fragment>
+          <p className={classNames(styles.title)}>Ends in</p>
+          <div className={classNames(styles.timer)}>
+            <Countdown date={onTimeEnd} />
+          </div>
+        </React.Fragment>
+      ) : (
+        <React.Fragment></React.Fragment>
+      )}
+    </div>
+  );
 }

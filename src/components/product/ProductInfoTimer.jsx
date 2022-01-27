03b4490ace3890a702e87b94978c188fcd56f863
Vasil Vasilev <backort@abv.bg>
2022-01-27 21:27:03 +0200

    a

--- src/components/product/ProductInfoTimer.jsx
+++ src/components/product/ProductInfoTimer.jsx
@@ -10,18 +10,17 @@ export default function ProductInfoTimer({ timeEnd, onTimeEnd }) {
         <div className={classNames(styles.timer)}></div>
       </div>
     );
-  } else {
-    return (
-      <div
-        className={classNames(styles["product-info-timer"], {
-          [styles.active]: timeEnd,
-        })}
-      >
-        <div className={classNames(styles.timer, { [styles.active]: timeEnd })}>
-          <p className={classNames(styles.title)}>Ends in</p>
-          <Countdown date={onTimeEnd} />
-        </div>
-      </div>
-    );
   }
+  return (
+    <div
+      className={classNames(styles["product-info-timer"], {
+        [styles.active]: timeEnd,
+      })}
+    >
+      <div className={classNames(styles.timer, { [styles.active]: timeEnd })}>
+        <p className={classNames(styles.title)}>Ends in</p>
+        <Countdown date={onTimeEnd} />
+      </div>
+    </div>
+  );
 }

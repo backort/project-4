80087d9a71dec734e11b3b8105dba49d153cd44b
Vasil Vasilev <backort@abv.bg>
2022-02-02 23:41:58 +0200

    a

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,10 +2,10 @@ import styles from './Hero.module.scss';
 import classNames from 'classnames';
 import { Typography } from '@mui/material';
 
-export default function Hero() {
+export default function Hero({text}) {
   return (
       <div className={classNames(styles.hero)}>
-          <Typography variant='h1' className={classNames(styles.text)}></Typography>
+          <Typography variant='h1' className={classNames(styles.text)}>{text}</Typography>
       </div>
   );
 }

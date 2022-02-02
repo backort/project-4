f7c4b4ad9ae58a14a38fe99bb180cc46c33b286f
Vasil Vasilev <backort@abv.bg>
2022-02-03 00:27:42 +0200

    a

---
+++ src/components/profilehero/ProfileHero.jsx
@@ -0,0 +1,11 @@
+
+import styles from "./ProfileHero.module.scss";
+import classNames from "classnames";
+
+export default function ProfileHero({image}) {
+  return (
+    <div className={classNames(styles['profile-hero'])}>
+        <img src={image} className={classNames(styles.image)}  />
+    </div>
+  );
+}

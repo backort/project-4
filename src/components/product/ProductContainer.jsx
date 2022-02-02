725b0f812de3a110de64ed7b2b5af166941da94f
Vasil Vasilev <backort@abv.bg>
2022-02-02 22:38:44 +0200

    a

--- src/components/product/ProductContainer.jsx
+++ src/components/product/ProductContainer.jsx
@@ -4,6 +4,7 @@ import ProductImage from "./ProductImage";
 import ProductInfo from "./ProductInfo";
 import ProductTabs from "./ProductTabs";
 import ProductActions from "./ProductActions";
+import { Grid } from "@mui/material";
 
 export default function ProductContainer({
   name,
@@ -17,33 +18,35 @@ export default function ProductContainer({
   source,
 }) {
   return (
-    <Grid container className={classNames(styles["product-container"])}>
-      <Grid item xs={6}>
-        <ProductImage url={source} />
+    <div className={classNames(styles["product-container"])}>
+      <Grid container>
+        <Grid item xs={6}>
+          <ProductImage url={source} />
+        </Grid>
+        <Grid item xs={5}>
+          <ProductInfo
+            title={name}
+            creator={owner}
+            price={price}
+            currency={currency}
+            likes={likes}
+            timeEnd={auction_end}
+            isLive={details.isLive}
+            onTimeEnd={auction_end}
+          />
+          <ProductTabs />
+          <ProductActions
+            bids={bids}
+            text={details}
+            isLive={details.isLive}
+            currency={currency}
+            buyAmount={bids.buyAmount}
+            bidAmount={bids.bidAmount}
+            onBid={bids.onBid}
+            onBuy={bids.onBuy}
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={5}>
-        <ProductInfo
-          title={name}
-          creator={owner}
-          price={price}
-          currency={currency}
-          likes={likes}
-          timeEnd={auction_end}
-          isLive={details.isLive}
-          onTimeEnd={auction_end}
-        />
-        <ProductTabs />
-        <ProductActions
-          bids={bids}
-          text={details}
-          isLive={details.isLive}
-          currency={currency}
-          buyAmount={bids.buyAmount}
-          bidAmount={bids.bidAmount}
-          onBid={bids.onBid}
-          onBuy={bids.onBuy}
-        />
-      </Grid>
-    </Grid>
+    </div>
   );
 }

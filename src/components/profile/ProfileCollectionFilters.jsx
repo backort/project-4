e7e2b0794f33c30e44d82bacc3cffef94978079e
Vasil Vasilev <backort@abv.bg>
2022-02-03 10:40:17 +0200

    a

--- src/components/profile/ProfileCollectionFilters.jsx
+++ src/components/profile/ProfileCollectionFilters.jsx
@@ -21,43 +21,46 @@ export default function ProfileCollectionFilters({ filters }) {
   const handlePrice = (e) => {
     setPrice(e.target.value);
   };
+
   return (
     <div className={classNames(styles["profile-collection-filters"])}>
-      <Stack direction="row" spacing={2}>
-        <FormControl fullWidth>
-          <InputLabel>Sort by</InputLabel>
-          <Select label="Sort by" value={sort} onChange={handleSort}>
-            {filters.sort.map((s) => (
-              <MenuItem key={s.value} value={s.value}>
-                {s.label}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl fullWidth>
-          <InputLabel>Price range</InputLabel>
-          <Select label="Price range" value={price} onChange={handlePrice}>
-            {filters.price.map((p) => (
-              <MenuItem key={p.value} value={p.value}>
-                {p.label}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl fullWidth>
-          <TextField
-            label="Search"
-            variant="standard"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon style={{ color: "white" }} />
-                </InputAdornment>
-              ),
-            }}
-          />
-        </FormControl>
-      </Stack>
+      {filters && (
+        <Stack direction="row" spacing={2}>
+          <FormControl fullWidth>
+            <InputLabel>Sort by</InputLabel>
+            <Select label="Sort by" value={sort} onChange={handleSort}>
+              {filters.sort.map((s) => (
+                <MenuItem key={s.value} value={s.value}>
+                  {s.label}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl fullWidth>
+            <InputLabel>Price range</InputLabel>
+            <Select label="Price range" value={price} onChange={handlePrice}>
+              {filters.price.map((p) => (
+                <MenuItem key={p.value} value={p.value}>
+                  {p.label}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl fullWidth>
+            <TextField
+              label="Search"
+              variant="standard"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon style={{ color: "white" }} />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </FormControl>
+        </Stack>
+      )}
     </div>
   );
 }

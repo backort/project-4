039da56f99813a05d11a48f51877ec5a4e4f1979
Vasil Vasilev <backort@abv.bg>
2022-02-03 09:55:03 +0200

    aa

---
+++ pages/profile/index.jsx
@@ -0,0 +1,31 @@
+import Header from "../../src/components/header/Header";
+import ProfileHero from "../../src/components/profile/ProfileHero";
+import ProfileUser from "../../src/components/profile/ProfileUser";
+import ProfileCollection from "../../src/components/profile/ProfileCollection";
+import Footer from "../../src/components/footer/Footer";
+import profileData from "../../data/profile.json";
+import { useState, useEffect } from "react";
+export default function profile() {
+  const [profile, setProfile] = useState([]);
+  useEffect(() => {
+    setProfile(profileData);
+  }, []);
+  return (
+    <>
+      <Header />
+      <ProfileHero image={profile.avatar?.backgroundUrl} />
+      <ProfileUser
+        name={profile.username}
+        info={profile.info}
+        avatar={profile.avatar?.url}
+        verified={profile.verified}
+      />
+      <ProfileCollection
+        user={profile}
+        filters={filters}
+        items={profile.nfts}
+      />
+      <Footer />
+    </>
+  );
+}

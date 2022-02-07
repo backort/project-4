82c06366f6c218c86d4061646cffe65bf7555044
Vasil Vasilev <backort@abv.bg>
2022-02-07 20:02:42 +0200

    a

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -4,18 +4,19 @@ import ActivityListItem from "./ActivityListItem";
 import { Stack } from "@mui/material";
 
 export default function ActivityList({ items }) {
-    console.log(items);
   return (
-    <Stack spacing={2} className={classNames(styles["activity-list"])}>
-      {items.map((item, index) => (
-        <ActivityListItem
-          key={index}
-          user={item.user}
-          created_at={item.created_at}
-          nft={item.nft}
-          type={item.type}
-        />
-      ))}
-    </Stack>
+    <div className={classNames(styles["activity-list"])}>
+      <Stack spacing={2}>
+        {items.map((item, index) => (
+          <ActivityListItem
+            key={index}
+            user={item.user}
+            created_at={item.created_at}
+            nft={item.nft}
+            type={item.type}
+          />
+        ))}
+      </Stack>
+    </div>
   );
 }
